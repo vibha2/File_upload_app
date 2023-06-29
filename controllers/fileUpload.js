40981922ca0b5735c32a84fe278cbada4d3fceab
vibha2 <juhisahu2203@gmail.com>
2023-06-30 03:34:22 +0530

    wip

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -6,8 +6,9 @@ exports.localFileUpload = async (req, res) => {
     try{
         //fetch file, jo bhi send ki hogi
         const file = req.files.file;
-        console.log("File Aagyi hai=> ",file);
+        console.log("File Aagyi hai-> ",file);
 
+        // __dirname => current directory
         // here path = server path
         // We mention Date.now() here because it gives every time a diff number,
         // So, we'll get different path everytime
