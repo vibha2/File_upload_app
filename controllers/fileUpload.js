17f855ac1f7de5e838902c59bd9a07ae5f29d770
vibha2 <juhisahu2203@gmail.com>
2023-07-04 02:09:53 +0530

    Reduce Image Uploaded to Cloudinary Successfully

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -35,12 +35,16 @@ function isFileTypeSupported(type, supportedTypes) {
     return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder)
+async function uploadFileToCloudinary(file, folder, quality)
 {
     const options = { folder };
     //using this uploader method we can upload file to cloudinary
     console.log("\ntempFilePath: ", file.tempFilePath);
+    if(quality) {
+        options.quality = quality;
+    }
     options.resource_type = "auto";
+
     return await cloudinary.uploader.upload(file.tempFilePath, options);
     
 }
@@ -164,6 +168,68 @@ exports.videoUpload = async (req, res) => {
     }
 }
 
+//TODO: Compress using height attribute
+
+//ImageReduce upload ka handler
+exports.imageReducerUpload = async (req, res) => {
+    try
+    {
+        //data fetch
+        const { name, tags, email} = req.body;
+        console.log("\nname: ",name);
+        console.log("\ntags: ",tags);
+        console.log("\nemail: ",email);
+        
+        const file = req.files.imageFile;
+
+       //validation
+       const supportedTypes = ["jpg", "jpeg", "png"];
+       //current file type
+       const fileType = file.name.split('.')[1].toLowerCase();
+       console.log("\nfileType: ", typeof(fileType));
+
+
+        // add a upper limit of 5 mb
+        if(!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success:false,
+                message:'File format not supported',
+            })
+        }
+
+        //file format supported then upload in cloudinary
+        console.log("Uploading to cloudinary");
+        const response = await  uploadFileToCloudinary(file, "myTest", 30);
+        console.log("response: ",response);
+
+         //db me entry save krni hai
+         const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url,
+        })
+
+        res.json({
+            success:true,
+            imageUrl: response.secure_url,
+            message: 'Reduce Image Successfully Uploaded',
+        })
+
+
+
+
+    }
+    catch(error)
+    {
+        console.log("error: ",error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong',
+        });
+    }
+}
+
 //Testing
 //POST request
 //http://localhost:4000/api/v1/upload/localFileUpload
@@ -176,4 +242,17 @@ exports.videoUpload = async (req, res) => {
 // tags -> material
 // email -> vibha@gmail.com
 // imageFile -> photo.jpg
+
+//POST request
+//http://localhost:4000/api/v1/upload/videoUpload
+//Body-formdata key -> value
+// same for video
+
+// Read this: https://cloudinary.com/documentation/transformations_on_upload
+// we hav attribute name quality
+// https://cloudinary.com/documentation/image_optimization
+//POST request
+//http://localhost:4000/api/v1/upload/imageReducerUpload
+//Body-formdata key -> value
+// same for ImageReducer
 
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // const {imageUpload, videoUpload, imageReducerUpload, localFilUpload} = require("../controllers/fileUpload");
-const { localFileUpload, imageUpload, videoUpload } = require("../controllers/fileUpload");
+const { localFileUpload, imageUpload, videoUpload, imageReducerUpload } = require("../controllers/fileUpload");
 
 //api route
 router.post("/localFileUpload", localFileUpload );
 router.post("/imageUpload", imageUpload );
 router.post("/videoUpload", videoUpload );
+router.post("/imageReducerUpload", imageReducerUpload );
 
 module.exports =  router;
